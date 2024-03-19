6a167fb6a2b4e8d8404634aea57731ba1d04d0a4
ryanchiang <ryanschiang@gmail.com>
2024-03-19 13:49:30 -0700

    fix parent element selector

--- src/util/getContents.js
+++ src/util/getContents.js
@@ -1,16 +1,8 @@
 module.exports = function () {
     // Get parent chat container
-    const chatContainer = document.querySelector("[class*='hhDfMG']");
-
-    // Fallback classes
-    if (!chatContainer)
-        chatContainer = document.querySelector(
-            "[class*='sc-fHIGvW']"
-        );
-    if (!chatContainer)
-        chatContainer = document.querySelector(
-            "[class*='flex-1 flex flex-col gap-3 px-4 pt-16']"
-        );
+    const chatContainer = document.querySelector(
+        "div.flex-1.flex.flex-col.gap-3.px-4.pt-16"
+    );
 
     // Get chat title (if exists)
     const titleEle = document.querySelector(
